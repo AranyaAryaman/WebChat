fa618536e812e4163fbb06c28dcaa1813da125cd
AranyaAryaman <aryamanaranya@gmail.com>
2025-02-01 15:45:46 -0500

    Signup, Login & Logout

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";  
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-import {connectDB} from "./lib/db.js"
+import {connectDB} from "./lib/db.js";
+
 
 dotenv.config()
 const app = express();
