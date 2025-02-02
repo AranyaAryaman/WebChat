7ebefcb3331a4d060e934ea5d98bad64ea90b247
AranyaAryaman <aryamanaranya@gmail.com>
2025-02-02 13:42:53 -0500

    Update Profile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -92,7 +92,7 @@ export const updateProfile = async (req,res)=>{
             return res.status(400).json({message: "Profile pic is required"});
         }
 
-        const uploadResponse = await cloudinary.uploaader.upload(profilePic)
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
         const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
 
         res.status(200).json(updatedUser);
